f5b9e36e02532dd17cf8d49dd6409e972c1786ea
Carlos Gómez <drcgomez@hotmail.com>
2023-10-09 20:15:34 -0500

    Creación primera prueba

---
+++ StringManipulation.Tests/StringOperationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringManipulation.Tests
+{
+    public class StringOperationTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperation = new StringOperations();
+
+            var result = strOperation.ConcatenateStrings("Hello", "World");
+
+            Assert.Equal("Hello World", result);
+        }
+    }
+}
