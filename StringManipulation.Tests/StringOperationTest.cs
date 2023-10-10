3d876b744b13d27cb55977a764be2720ff37f38e
Carlos Gómez <drcgomez@hotmail.com>
2023-10-09 20:50:06 -0500

    Creando más pruebas
    Creación tipos de pruebas y validando tipos de assert

--- StringManipulation.Tests/StringOperationTest.cs
+++ StringManipulation.Tests/StringOperationTest.cs
@@ -12,11 +12,120 @@ namespace StringManipulation.Tests
         [Fact]
         public void ConcatenateStrings()
         {
+            // Arrange
             var strOperation = new StringOperations();
 
+            // Act
             var result = strOperation.ConcatenateStrings("Hello", "World");
 
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
             Assert.Equal("Hello World", result);
+        }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.IsPalindrome("ama");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.IsPalindrome("Hello");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void RemoveWhitespace()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.RemoveWhitespace("Hello World");
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal("HelloWorld", result);
+        }
+
+        [Fact]
+        public void QuantintyInWords()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.QuantintyInWords("perro", 2);
+
+            // Assert
+            Assert.StartsWith("dos", result);
+            Assert.Contains("perro", result);
+        }
+
+        [Fact]
+        public void GetStringLength()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var word = "Hello World";
+            var result = strOperation.GetStringLength(word);
+
+            // Assert
+            Assert.Equal(word.Length, result);
+        }
+
+        [Fact]
+        public void GetStringLength_Exception()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            Assert.Throws<ArgumentNullException>(() => strOperation.GetStringLength(null));
+        }
+
+        [Fact]
+        public void TruncateString()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.TruncateString("Test Truncate", 4);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal("Test", result);
+        }
+
+        [Fact]
+        public void TruncateString_Exception()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            Assert.Throws<ArgumentOutOfRangeException>(() => strOperation.TruncateString("Test Exception", -1));
         }
     }
 }
